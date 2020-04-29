95984023772c8ae6f6247abe93fc834680372822
fabezio <fabezio@outlook.fr>
2020-04-30 01:08:44 +0200

    ajout layouts error et white

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+div
+  b-alert.d-block(variant='warning' show)
+    span Oops
+
+  .float-right
+    nuxt-link(to='/') Retourner à l'accueil
+
+</template>
+
+<script>
+export default {
+  layout: 'white'
+}
+</script>
---
+++ layouts/white.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+.full-page
+  nuxt
+</template>
+<style lang="stylus">
+.full-page {
+  width: 100% !important;
+  margin: auto;
+  padding: auto;
+  min-height: 100vh;
+  display: inline-block;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
